50ac437fa4af8a904835d9bb3d78f747e6854973
Laura Ross <lauramarie100@gmail.com>
2022-06-12 01:44:53 -0400

    Setting up Election Result Analysis project files

---
+++ PyPoll.py
@@ -0,0 +1,11 @@
+# Data we need to retrieve from csv file
+# 1. The total number of votes cast.
+#
+# 2. A complete list of candidates who received votes.
+#
+# 3. The total number of votes each candidate received.
+#
+# 4. The percentage of votes each candidate won.
+#
+# 5. The winner of the election based on the popular votes.
+#
